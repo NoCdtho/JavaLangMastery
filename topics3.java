56521d1e3c7ee8417cb606b808bb9aae194e5375
Rohit Gupta <shecded@gmail.com>
2026-06-19 03:57:15 +0530

    con java records

--- topics3.java
+++ topics3.java
@@ -9,6 +9,9 @@
     are classes that are used to store multiple constants.
     5. interface:
     they are like classes but they don't have any implementation of the methods, they only contain signatures and fields
+    6.java records:
+    this is special class that defines immutable data-only. The java records consists one or more fields which acts as member/instance 
+    variables in a regular java class. And we don't have to write the boilerplate code to access them.
  */
 
 public class topics3 {
@@ -27,11 +30,15 @@ public class topics3 {
         System.out.println("For mars " + Solar.Moon);
 
         // Enum
+        // use of .valuesof()
+        Level obj2 = Level.valueOf("high");
+        System.out.println("Use of valuesOf func: " + obj2);
         Level level = Level.high;
         System.out.println(level);
         String val = level.write();
         System.out.println(val);
 
+        // Use of .values build-in method return the current constant.
         for (Level level2 :Level.values()){
             String newValue = level2.write();
             System.out.println(newValue);
@@ -40,7 +47,19 @@ public class topics3 {
         // Interface
         firstinterface obj = new useInterface();
         System.out.println(obj.sayhello());
-    }    
+
+        // Java records
+        Singer singer = new Singer("RAPPER","twentyThree");
+        Singer singer2 = new Singer("SLOW");
+
+        System.out.println("Printing the normal values of the recodes....");
+        System.out.println("First singer type: " + singer.type() +" and age: "+ singer.age());
+        System.out.println("second singer type: " + singer2.type() +" and age: No age mentioned"); 
+        System.out.println("Type of singer 2 changed from: " + singer2.type()+ " to: " + singer2.inSmallLetter());
+        System.out.println("Nd age of singer 1 change from " + singer.age() + " to " + Singer.toLargeLetter(singer));
+
+
+    }
 }
 
 class typeChecking{
@@ -121,8 +140,25 @@ interface firstinterface{
 
 // create a class to use the interface
 class useInterface implements firstinterface{
+
     @Override
     public String sayhello(){
         return myinterfaceValue;
     }
 }
+
+// Con Java records
+ record Singer(String type, String age){
+    // records can have multiple constructors
+    Singer(String type){
+        this(type, null);
+    }
+    //  methods 
+    String inSmallLetter(){
+        return type().toLowerCase();
+    } 
+    // static method
+    static String toLargeLetter(Singer singer ){
+        return singer.age.toUpperCase();
+    }
+} 
