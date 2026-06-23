7fa085ccd02737455f404dd5cf8f5aebbe851f4a
Rohit Gupta <shecded@gmail.com>
2026-06-23 17:53:50 +0530

    annotations

--- topics4.java
+++ topics4.java
@@ -2,7 +2,20 @@
 1. lamda expression: 
 This is used as functionl programming. It is used to implement in the place of unimplemented of a object (for interfaces). 
 This can written in place of both constructor and refence variable.
+
+2.annotation:
+This are used to give the give meta data of the source code
+mainly there are 4: @Deprecated, @override, @suppressWarnings, @Contended 
+I can create my own annotations also I will code only that.
+
 */
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Target;
 
 public class topics4 {
     public static void main(String[] args ){
@@ -40,3 +53,19 @@ public class topics4 {
     }
 };
 
+// Annotation
+@Retention(RetentionPolicy.CLASS) //This is the default retention annotation behaviour and there 2 more .RUNTIME and .SOURCE
+@Target({ElementType.TYPE}) //This is a default behaviour of Target annotation which is used to define where the annotation should be used.
+@Inherited //This is used to define that the subclasses of the superclass that is annotated by the below annotation can are annoted as well. 
+@Documented //This makes the classes that are annotated by this annotation are visible to the JAVADOC tool.
+@interface myAnnotation{
+    String name();
+    int age();
+    String[] Workedbefore();
+    String colleage() default "James";
+}
+
+@myAnnotation(name = "martin", age = 18, Workedbefore = "producer")
+class useMyAnnotaion{
+
+}
