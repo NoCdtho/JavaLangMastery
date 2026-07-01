ef852c8f2c00c9ea4366c836f09cade8f8601f74
Rohit Gupta <shecded@gmail.com>
2026-07-01 14:05:06 +0530

    added post method

--- Topics5.java
+++ Topics5.java
@@ -6,6 +6,7 @@
  I specifically studied How to make http get and post request and read and write the data on the server.
 */
 
+
 import java.io.*;
 import java.net.*;
 
@@ -17,6 +18,14 @@ public class Topics5 {
             write w = new write();
             read b = new read();
             System.out.println(b.a);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+        // API        
+        try{
+            HttpURLConnectionExample.sendget();
         }
         catch(Exception e){
             System.out.println(e);
@@ -58,8 +67,8 @@ class HttpURLConnectionExample{
     private final static String POST_URL = "randomURL/write"; //URL used to write data on.
     private final static String POST_PARAMS =  "userName=Rohit";
 
-    private static void sendget() throws IOException{
-        URL obj = new URL(GET_URL); //used to parse the URL 
+    public static void sendget() throws IOException{
+        URL obj = new URL(GET_URL); //used to parse the URL in simple language comverts the URL in java objects
         HttpURLConnection con = (HttpURLConnection) obj.openConnection(); //create the connection
         con.setRequestMethod("GET"); //used to send the get method request which is to retrive data from server
         con.setRequestProperty("USER_AGENT", USER_AGENT); //defines the request header like what platform, os the user is etc
@@ -79,5 +88,36 @@ class HttpURLConnectionExample{
         else{
             System.out.println("call get method again");
         }
+    }
+    public static void sendpost() throws Exception{
+        URL obj = new URL(POST_URL);
+        HttpURLConnection con =  (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("user-agent", USER_AGENT);
+
+        // tell application to send data to the server
+        con.setDoOutput(true); //create a output stream to write on the destination
+        OutputStream os =  con.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("THe connection status: " + responseCode);
+        if(responseCode == HttpURLConnection.HTTP_OK){
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while((inputLine = in.readLine()) != null){
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        }
+        else{
+            System.out.println("send post method again");
+        }
+
     }
 }
